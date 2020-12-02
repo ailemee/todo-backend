0dcfe3c95bf91876a378a536ac39d23623feceb3
Eemeli Allisto <emppua11@gmail.com>
2020-12-02 17:17:31 +0200

    Add initial structure and functionality for backend connection

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require("express");
+const connection = require("./database/crudrepository.js");
 const app = express();
 var cors = require("cors");
 app.use(cors());
 
-const db = [{ name: "tiina" }, { name: "jack" }];
+app.use(express.json());
 
-app.get("/names", (req, res) => {
-  res.send(db);
+// curl http://localhost:8080/todo
+app.get("/todo", (req, res) => {
+  connection.findAll((result) => res.send(result));
 });
 
+// curl http://localhost:8080/todo/id
+app.get("/todo/:taikaviitta([0-9]+)", (req, res) => {
+  const id = req.params.taikaviitta;
+  connection.findById(id, (todoItem) => res.send(todoItem));
+});
+
+// curl -X DELETE http://localhost:8080/todo/id
+app.delete("/todo/:taikaviitta([0-9]+)", (req, res) => {
+  const id = req.params.taikaviitta;
+  connection.deleteById(id, (todoItem) => res.send(todoItem));
+});
+
+app.post("/todo", (req, res) => {
+  const todoItem = {
+    name: req.body.name,
+    priority: req.body.priority,
+  };
+  connection.save(todoItem, (todoItem) => res.send(todoItem));
+});
+
+// const server = app.listen(8080, () => {
+//   console.log(`Listening on port ${server.address().port}`);
+// });
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`Listening on port ${server.address().port}`);
