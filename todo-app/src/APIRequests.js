d1b8dd90c29632ba07060b247d0f10b60833edc3
Eemeli Allisto <emppua11@gmail.com>
2020-12-17 20:56:04 +0200

    Fix typo in heroku url.

--- todo-app/src/APIRequests.js
+++ todo-app/src/APIRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://tamk-4a00ez62-3002-group14.herokuapp.com/todo";
+const API_URL = "https://tamk-4a00ez62-3002-group14.herokuapp.com/todo/";
 
 async function getAllTodos() {
   const { data: todo } = await axios.get(API_URL);
